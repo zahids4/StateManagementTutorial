14e231e11f8ab4f6475ce92fab127a517ed111ff
Saim Zahid <saimzahid@me.com>
2022-01-07 11:38:47 -0500

    Initial Commit

---
+++ StateManagementTutorial/StateManagementTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementTutorialApp.swift
+//  StateManagementTutorial
+//
+//  Created by Saim Zahid on 2022-01-07.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
